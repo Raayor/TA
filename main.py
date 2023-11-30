c37ad212838ad1b92c63ff7a71fe88872cfc55ec
Raayor <152498157+Raayor@users.noreply.github.com>
2023-11-30 20:08:12 +0700

    Add files via upload

--- main.py
+++ main.py
@@ -3,11 +3,10 @@ import datetime
 
 tab1, tab2, tab3 = st.tabs(["Home", "Peminjaman", "Pengembalian"])
 with tab1:
-   st.write('pinjam')
+   st.header('pinjam')
+   st.input('Masukkan kode buku')
 with tab2:
-   st.header("A dog")
-   st.image("https://static.streamlit.io/examples/dog.jpg", width=200)
+   st.header('Peminjaman')
 with tab3:
-   st.header("An owl")
-   st.image("https://static.streamlit.io/examples/owl.jpg", width=200)
+   st.header("Pengembalian")
 
